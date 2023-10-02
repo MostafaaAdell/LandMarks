1008435db0dc9419a250f77654f76240010ebdfd
MustafaaAdell <Mustafaadell9777@gmail.com>
2023-10-02 12:54:03 +0300

    Initial Commit

---
+++ SwiftUIDayOne/SwiftUIDayOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDayOneApp.swift
+//  SwiftUIDayOne
+//
+//  Created by Mustafa on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDayOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
